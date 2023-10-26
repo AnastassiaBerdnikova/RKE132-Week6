f263083ab3e2371c0971829d95651f3fa07ece3c
Anastassia Brd. <anastassia.berdnikova@gmail.com>
2023-10-26 19:02:09 +0300

    add week6 code

---
+++ ArrayWithRandom/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] snacks = { "sushi", "pizza", "burger", "thai", "ramen" };
+
+Random rnd = new Random();
+
+int randomIndex = rnd.Next(0, snacks.Length);
+
+Console.WriteLine($"Tonight we are going to eat {snacks[randomIndex]}.");
+
+string[] colors = { "red", "green", "blue", "pink", "yellow" };
+for (int i = colors.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine($"{colors.Length - i}.{colors[i]}");
+}
